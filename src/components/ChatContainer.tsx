2aaacd02811a0761a8d30e5053074f92e3636968
robiulsagor <robiul100.me@gmail.com>
2025-05-20 12:23:14 +0600

    UI: ChatContainer - Show messages design

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,16 +1,11 @@
-import { AiOutlineVerticalRight } from "react-icons/ai";
 import { BiInfoCircle, BiSend } from "react-icons/bi";
 import { CgAttachment } from "react-icons/cg";
-import { GrAttachment } from "react-icons/gr";
-import { ImAttachment } from "react-icons/im";
-import { MdImageAspectRatio } from "react-icons/md";
-import { RiAttachmentFill } from "react-icons/ri";
 
 const ChatContainer = ({ selectedChat, showChatInfo, setShowChatInfo }) => {
     console.log("selectedChat", selectedChat);
 
     return (
-        <div className="relative flex flex-col ">
+        <div className="relative flex flex-col justify-between  max-h-full overflow-hidden">
             {/* header part */}
             <div className="flex items-center justify-between py-3 px-3 border-b border-slate-600">
                 <div className="flex items-center gap-3">
@@ -32,11 +27,90 @@ const ChatContainer = ({ selectedChat, showChatInfo, setShowChatInfo }) => {
             {/* header part ends */}
 
             {/* messages */}
-            <div className="flex-1">
-                <h2>hi there</h2>
+            <div className="overflow-y-scroll flex-1 py-2 flex flex-col-reverse gap-4">
+
+                {/* msg */}
+                <div className="flex gap-4 items-end max-w-1/2">
+                    <img src={selectedChat.picture} className="w-10 h-10 rounded-full mb-5" alt="" />
+                    <div>
+                        <p className="bg-cyan-700/50 p-3 rounded-2xl">
+                            and God will make you successful.
+                        </p>
+                        <span className="text-gray-700 text-sm"> Yesterday </span>
+                    </div>
+                </div>
+
+                {/* msg */}
+                <div className="flex gap-4 items-end max-w-1/2">
+                    <img src={selectedChat.picture} className="w-10 h-10 rounded-full mb-5" alt="" />
+                    <div>
+                        <p className="bg-cyan-700/50 p-3 rounded-2xl">
+                            Maybe you should try some more
+                        </p>
+                        <span className="text-gray-700 text-sm"> Yesterday </span>
+                    </div>
+                </div>
+
+                <div className="flex gap-4 items-end max-w-1/2">
+                    <img src={selectedChat.picture} className="w-10 h-10 rounded-full mb-5" alt="" />
+                    <div>
+                        <p className="bg-cyan-700/50 p-3 rounded-2xl">
+                            Oh no! What are you saying?
+                            Maybe you are struggling, but you are not a failure.
+                            Maybe you should try some more, and God will make you successful.
+                        </p>
+                        <span className="text-gray-700 text-sm"> Yesterday </span>
+                    </div>
+                </div>
+
+                {/* msg */}
+                <div className="flex gap-4 items-end flex-row-reverse ml-auto max-w-1/2">
+                    <img src={selectedChat.picture} className="w-10 h-10 rounded-full mb-5" alt="" />
+                    <div>
+                        <p className="bg-cyan-700/50 p-3 rounded-2xl">
+                            I'm just sitting aimlessly, thinking what to do, how to do, and when to do!
+                            I'm such a failure, I should have died!
+                        </p>
+                        <span className="text-gray-700 text-sm"> Yesterday </span>
+                    </div>
+                </div>
+
+                {/* msg revs */}
+                <div className="flex gap-4 items-end max-w-1/2 ">
+                    <img src={selectedChat.picture} className="w-10 h-10 rounded-full mb-5" alt="" />
+                    <div>
+                        <p className="bg-cyan-700/50 p-3 rounded-2xl">
+                            I'm good. What are you doing?
+                        </p>
+                        <span className="text-gray-700 text-sm"> Yesterday </span>
+                    </div>
+                </div>
+                {/* msg revs */}
+                <div className="flex flex-row-reverse gap-4 items-end max-w-1/2 ml-auto">
+                    <img src={selectedChat.picture} className="w-10 h-10 rounded-full mb-5" alt="" />
+                    <div>
+                        <p className="bg-cyan-700/50 p-3 rounded-2xl">
+                            Hi! I'm fine. How are you?
+                        </p>
+                        <span className="text-gray-700 text-sm"> Yesterday </span>
+                    </div>
+                </div>
+
+                {/* msg */}
+                <div className="flex gap-4 items-end max-w-1/2">
+                    <img src={selectedChat.picture} className="w-10 h-10 rounded-full mb-5" alt="" />
+                    <div>
+                        <p className="bg-cyan-700/50 p-3 rounded-2xl">
+                            Hi there! How are you?
+                        </p>
+                        <span className="text-gray-700 text-sm"> Yesterday </span>
+                    </div>
+                </div>
+
+
             </div>
 
-            <div className={`flex items-center gap-4 mb-3 ${showChatInfo ? 'pr-0' : 'pr-3'}`}
+            <div className={`w-full flex items-center gap-4 mb-3 ${showChatInfo ? 'pr-0' : 'pr-3'}`}
 
             >
                 <div className="flex-1 flex items-center gap-3 border border-gray-600 rounded-full px-6 bg-slate-400/10">
