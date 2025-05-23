3f1fdc962c6884ce9fe93953503d35b5cc5ca9ae
robiulsagor <robiul100.me@gmail.com>
2025-05-23 12:30:47 +0600

    fix: Proxy issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000/api/',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
         ws: true,
+         rewrite: (path) => path.replace(/^\/api/, '/api/v1')
       },
     },
   }
