bb399ded61a2bb012df588e6371fce4acd353e26
robiulsagor <robiul100.me@gmail.com>
2025-05-27 09:57:08 +0600

    fix: Add loading state management to chatSlice and update related selectors

--- src/redux/chatSlice.ts
+++ src/redux/chatSlice.ts
@@ -4,30 +4,34 @@ import type { RootState } from "./store"
 const initialState = {
     chatList: [],
     selectedChat: null,
-    messages: []
+    messages: [],
+    loading: true
 }
 
 const chatSlice = createSlice({
     name: 'chat',
     initialState,
     reducers: {
-        setChatList: (state,action) => {
+        setLoading: (state, action) => {
+            state.loading = action.payload
+        },
+        setChatList: (state, action) => {
             state.chatList = action.payload
         },
-        selectChat: (state, action)=> {
-            console.log(action);
-            
+        selectChat: (state, action) => {
             state.selectedChat = action.payload
         },
-        setMessages: (state, action)=> {
+        setMessages: (state, action) => {
             state.messages = action.payload
+            state.loading = false
         }
     }
 })
 
-export const {setChatList, selectChat, setMessages} = chatSlice.actions
-export const getChatList = (state: RootState)=> state.chat.chatList
-export const getSelectedChat = (state: RootState)=> state.chat.selectedChat
-export const getMessages = (state: RootState)=> state.chat.messages
+export const { setLoading, setChatList, selectChat, setMessages } = chatSlice.actions
+export const getLoading = (state: RootState) => state.chat.loading
+export const getChatList = (state: RootState) => state.chat.chatList
+export const getSelectedChat = (state: RootState) => state.chat.selectedChat
+export const getMessages = (state: RootState) => state.chat.messages
 
 export default chatSlice.reducer
